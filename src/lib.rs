f5f8695f916b914a1d8a759dd2caaaceb7d80afd
Aleksei Voronov <despawn@gmail.com>
2018-01-01 14:09:18 +0100

    Properly separate and keep punctuation

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,8 @@ extern crate regex;
 #[macro_use]
 extern crate serde_derive;
 extern crate serde;
-
+#[macro_use]
+extern crate lazy_static;
 
 use std::collections::HashMap;
 use std::fs::File;
@@ -13,6 +14,10 @@ use std::io::Read;
 use failure::Error;
 use itertools::Itertools;
 use regex::Regex;
+
+lazy_static!{
+    static ref WORD_SEPARATORS: Regex = Regex::new(r"[,.?!:;\s]+").unwrap();
+}
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Model {
@@ -34,7 +39,8 @@ impl Model {
         let mut trigram_stats = CaseStats::new();
 
         for sentence in text.lines().filter(|s| is_sentence_sane(s)) {
-            let tokens: Tokens = tokenize(sentence).into_iter().map(Token::from).collect();
+            let mut tokens = tokenize(sentence);
+            tokens.retain(|t| t.is_meaningful);
 
             for token in tokens.iter().cloned() {
                 unigram_stats.add_token(token);
@@ -52,43 +58,40 @@ impl Model {
     }
 
     pub fn truecase(&self, sentence: &str) -> String {
-        let words: Vec<_> = tokenize(sentence).into_iter().map(|s| normalize(&s)).collect();
+        let tokens = tokenize(sentence);
+        let total_tokens = tokens.len();
 
-        let mut result: Vec<Option<String>> = Vec::with_capacity(words.len());
-        for _ in 0..words.len() {
-            result.push(None);
-        }
+        let words_with_indexes: Vec<_> = tokens.iter().enumerate().filter(|x| x.1.is_meaningful).collect();
 
-        for (index, trigram) in words.windows(3).enumerate() {
-            let trigram_string = trigram.iter().join(" ");
-            if let Some(truecased) = self.trigrams.get(&trigram_string) {
-                for (pos, word) in truecased.split(" ").enumerate() {
-                    if result[index+pos].is_none() {
-                        result[index+pos] = Some(word.to_owned());
+        let mut truecase_tokens: Vec<Option<String>> = Vec::with_capacity(total_tokens);
+        truecase_tokens.resize(total_tokens, None);
+
+        let select_true_case_from_ngrams = |size, source: &CaseMap, result: &mut Vec<Option<String>>| {
+            for slice in words_with_indexes.windows(size) {
+                let indexes = slice.iter().map(|x| x.0);
+                let ngram = slice.iter().map(|x| &x.1.normalized).join(" ");
+                if let Some(truecased_ngram) = source.get(&ngram) {
+                    for (word, index) in truecased_ngram.split(" ").zip(indexes) {
+                        result[index].get_or_insert_with(|| word.to_owned());
                     }
                 }
             }
-        }
+        };
 
-        for (index, bigram) in words.windows(2).enumerate() {
-            let bigram_string = bigram.iter().join(" ");
-            if let Some(truecased) = self.bigrams.get(&bigram_string) {
-                for (pos, word) in truecased.split(" ").enumerate() {
-                    if result[index+pos].is_none() {
-                        result[index+pos] = Some(word.to_owned());
-                    }
-                }
+        select_true_case_from_ngrams(3, &self.trigrams, &mut truecase_tokens);
+        select_true_case_from_ngrams(2, &self.bigrams, &mut truecase_tokens);
+
+        for (truecase, token) in truecase_tokens.iter_mut().zip(tokens.iter()) {
+            if token.is_meaningful && truecase.is_none() {
+                *truecase = self.unigrams.get(&token.normalized).map(String::clone);
+            }
+
+            if truecase.is_none() {
+                *truecase = Some(token.original.clone());
             }
         }
 
-        for (index, word) in words.iter().enumerate() {
-            if result[index].is_none() {
-                let truecased = self.unigrams.get(word).unwrap_or(word).to_owned();
-                result[index] = Some(truecased);
-            }
-        }
-
-        result.into_iter().map(Option::unwrap).join(" ")
+        truecase_tokens.into_iter().map(Option::unwrap).join("")
     }
 }
 
@@ -123,6 +126,7 @@ impl CaseStats {
             self.add_token(Token {
                 original,
                 normalized,
+                is_meaningful: true
             });
         }
     }
@@ -148,14 +152,17 @@ type Tokens = Vec<Token>;
 struct Token {
     pub original: String,
     pub normalized: String,
+    pub is_meaningful: bool,
 }
 
-impl From<String> for Token {
-    fn from(original: String) -> Token {
+impl Token {
+    fn new(original: &str, is_meaningful: bool) -> Self {
+        let original = original.to_owned();
         let normalized = normalize(&original);
-        Token {
+        Self {
             original,
             normalized,
+            is_meaningful,
         }
     }
 }
@@ -164,12 +171,27 @@ fn is_sentence_sane(sentence: &str) -> bool {
     !sentence.chars().all(char::is_uppercase)
 }
 
-fn tokenize(sentence: &str) -> Vec<String> {
-    // TODO: split punctuation into separate tokens
-    sentence
-        .split_whitespace()
-        .map(str::to_owned)
-        .collect()
+fn tokenize(sentence: &str) -> Tokens {
+    let mut tokens = Vec::new();
+
+    let mut string = sentence;
+    while let Some(mat) = WORD_SEPARATORS.find(string) {
+        let (before, matching_part, rest) = split_in_three(string, mat.start(), mat.end());
+        tokens.push(Token::new(before, true));
+        tokens.push(Token::new(matching_part, false));
+        string = rest;
+    }
+
+    tokens.push(Token::new(string, true));
+    tokens.retain(|token| !token.original.is_empty());
+
+    tokens
+}
+
+fn split_in_three(string: &str, index1: usize, index2: usize) -> (&str, &str, &str) {
+    let (first, rest) = string.split_at(index1);
+    let (second, third) = rest.split_at(index2 - index1);
+    (first, second, third)
 }
 
 fn normalize(token: &str) -> String {
