8bdd8c75b0bf888dc1ee31d38cd4fe7525eed489
Aleksei Voronov <despawn@gmail.com>
2018-02-09 20:42:28 +0100

    Add quotes, dashes and parentheses to word separators

--- src/lib.rs
+++ src/lib.rs
@@ -268,7 +268,7 @@ fn tokenize(sentence: &str) -> Tokens {
 }
 
 lazy_static!{
-    static ref WORD_SEPARATORS: Regex = Regex::new(r"[,.?!:;\s]+").unwrap();
+    static ref WORD_SEPARATORS: Regex = Regex::new(r#"[,.?!:;()«»“”"—\s]+"#).unwrap();
 }
 
 struct Tokenizer<'a> {
